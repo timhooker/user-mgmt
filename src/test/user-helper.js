3465c563b549f05b0ca2ebaaa8cfa830c599f9e8
Tim Hooker <timhooker@me.com>
2015-02-01 16:08:29 -0500

    Update 2015-02-01T21:08:24.797Z

--- src/test/user-helper.js
+++
@@ -1,7 +0,0 @@
-function makeUser(firstName, lastName, email) {
-  return User({
-    firstName: firstName || 'Chris',
-    lastName: lastName || 'Davies',
-    email: email || 'chris.davies@example.com'
-  });
-}
